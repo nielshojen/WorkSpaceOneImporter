e7231d227b0441151c25fa1bc7018baad2fe1eba
codeskipper <12513894+codeskipper@users.noreply.github.com>
2024-07-03 12:10:02 +0200

    debug parsing ws1_app_versions_to_keep from env in ws1_app_versions_prune() to prevent "expected string or bytes-like object" error

--- WorkSpaceOneImporter/WorkSpaceOneImporter.py
+++ WorkSpaceOneImporter/WorkSpaceOneImporter.py
@@ -1011,7 +1011,8 @@ class WorkSpaceOneImporter(Processor):
     def ws1_app_versions_prune(self, api_base_url, headers, app_name, search_results):
 
         # get ws1_app_versions_to_keep, defaults to 5
-        keep_versions = extract_first_integer_from_string(self.env.get("ws1_app_versions_to_keep", 5))
+        keep_versions_str = self.env.get("ws1_app_versions_to_keep", "5")
+        keep_versions = extract_first_integer_from_string(keep_versions_str)
         if keep_versions < 1 or keep_versions > 10:
             self.output(f"{type(keep_versions)}", verbose_level=4)
             self.output(f"ws1_app_versions_to_keep setting {keep_versions:d} is out or range, setting default of 5.")
